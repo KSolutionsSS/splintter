8e1a1e2b48c339d61c49c55b6db99c7248183a53
Nahuel Barrios <barrios.nahuel@gmail.com>
2016-07-16 17:27:14 -0300

    Disable static-code-analysis plugin in order to test data binding

--- app/src/main/java/com/ksss/splintter/features/home/HomeActivity.java
+++ app/src/main/java/com/ksss/splintter/features/home/HomeActivity.java
@@ -1,9 +1,11 @@
 package com.ksss.splintter.features.home;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import com.ksss.splintter.R;
+import com.ksss.splintter.databinding.HomeLayoutBinding;
 
 /**
  * Created by Nahuel Barrios on 7/16/16.
@@ -14,6 +16,7 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.home_layout);
+        HomeLayoutBinding binding = DataBindingUtil.setContentView(this, R.layout.home_layout);
+        binding.withDataBinding.setText(R.string.using_data_binding_value);
     }
 }
