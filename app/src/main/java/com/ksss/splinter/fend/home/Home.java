8bd67fa48aafe94694d4d4499bcf4c0489925a7a
Nahuel Barrios <barrios.nahuel@gmail.com>
2016-07-16 11:24:13 -0300

    Create application

---
+++ app/src/main/java/com/ksss/splinter/fend/home/Home.java
@@ -0,0 +1,20 @@
+package com.ksss.splinter.fend.home;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.ksss.splinter.R;
+
+/**
+ * Created by nbarrios on 7/16/16.
+ */
+
+public class Home extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.home_layout);
+    }
+}
