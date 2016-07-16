f2a97e19debbeeed6796c2051d21157b65bd03ea
Nahuel Barrios <barrios.nahuel@gmail.com>
2016-07-16 16:02:02 -0300

    Add Timber with its default DEBUG tree for debug variant

--- app/src/main/java/com/ksss/splintter/MainApplication.java
+++ app/src/main/java/com/ksss/splintter/MainApplication.java
@@ -2,10 +2,17 @@ package com.ksss.splintter;
 
 import android.app.Application;
 
+import timber.log.Timber;
+
 /**
  * Created by Nahuel Barrios on 7/16/16.
  */
 
 public class MainApplication extends Application {
 
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Timber.d("Creating MAIN application...");
+    }
 }
