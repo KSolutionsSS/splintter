a2365b0fa9c262d37b40cf513d26ec463ee4403e
Nahuel Barrios <barrios.nahuel@gmail.com>
2016-07-16 16:18:47 -0300

    Fix HomeActivity.java parent class to include action bar

--- app/src/main/java/com/ksss/splintter/features/home/HomeActivity.java
+++ app/src/main/java/com/ksss/splintter/features/home/HomeActivity.java
@@ -1,15 +1,14 @@
 package com.ksss.splintter.features.home;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import com.ksss.splintter.R;
 
 /**
  * Created by Nahuel Barrios on 7/16/16.
  */
-
-public class HomeActivity extends Activity {
+public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
