05211d64477863e09ea3800579a66fa862b955d2
Nahuel Barrios <barrios.nahuel@gmail.com>
2016-07-17 11:07:21 -0300

    Create Intents.java enum to manage all deep links from once place to prevent duplicates

---
+++ app/src/main/java/com/ksss/splintter/Intents.java
@@ -0,0 +1,29 @@
+package com.ksss.splintter;
+
+import android.net.Uri;
+import android.support.annotation.NonNull;
+
+import com.ksss.splintter.util.SafeIntent;
+
+/**
+ * Enumerates the different deep links that this application currently supports.
+ * <p>
+ * Created by Nahuel Barrios on 7/16/16.
+ */
+public enum Intents {
+
+    GROUPS_CREATE("group/create");
+
+    public static final String SCHEME = "splintter://";
+
+    private final String hostAndPath;
+
+    Intents(String hostAndPath) {
+        this.hostAndPath = hostAndPath;
+    }
+
+    @NonNull
+    public SafeIntent get() {
+        return new SafeIntent(Uri.parse(SCHEME + hostAndPath));
+    }
+}
