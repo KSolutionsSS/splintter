30f1f6430a34a814f7cca81d32893d16d25089d4
Nahuel Barrios <barrios.nahuel@gmail.com>
2016-07-17 11:06:37 -0300

    Create SafeIntent.java

---
+++ app/src/main/java/com/ksss/splintter/util/SafeIntent.java
@@ -0,0 +1,16 @@
+package com.ksss.splintter.util;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+
+/**
+ * Created by Nahuel Barrios on 7/16/16.
+ */
+public class SafeIntent extends Intent {
+
+    public SafeIntent(@NonNull Uri uri) {
+        setPackage(getPackage());
+        setData(uri);
+    }
+}
