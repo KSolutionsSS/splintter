89e6c3f3f7345910f6c656820de1bbba1ef5ceea
Nahuel Barrios <barrios.nahuel@gmail.com>
2016-07-17 12:14:17 -0300

    Add PagerTitleStripe to the Group's view

--- app/src/main/java/com/ksss/splintter/features/group/GroupActivity.java
+++ app/src/main/java/com/ksss/splintter/features/group/GroupActivity.java
@@ -3,6 +3,9 @@ package com.ksss.splintter.features.group;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -10,7 +13,8 @@ import android.widget.EditText;
 
 import com.ksss.splintter.R;
 import com.ksss.splintter.features.group.domain.Group;
-import com.ksss.splintter.features.group.view.GroupPagerAdapter;
+import com.ksss.splintter.features.group.view.GroupExpensesFragment;
+import com.ksss.splintter.features.group.view.GroupExpensesSummaryFragment;
 
 import timber.log.Timber;
 
@@ -92,6 +96,55 @@ public class GroupActivity extends AppCompatActivity {
                         onBackPressed();
                     }
                 }).show();
+    }
+
+    private class GroupPagerAdapter extends FragmentPagerAdapter {
+
+        private GroupPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public int getCount() {
+            return 2;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            Fragment result;
+
+            switch (position) {
+                case 0:
+                    result = new GroupExpensesFragment();
+                    break;
+                case 1:
+                    result = new GroupExpensesSummaryFragment();
+                    break;
+                default:
+                    Timber.e("It's all wrong!!");
+                    result = new GroupExpensesFragment();
+            }
+
+            return result;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            int result;
+            switch (position) {
+                case 0:
+                    result = R.string.view_group_expenses_pager_title;
+                    break;
+                case 1:
+                    result = R.string.view_group_expenses_summary_pager_title;
+                    break;
+                default:
+                    Timber.e("It's all wrong!!");
+                    result = R.string.view_group_expenses_pager_title;
+            }
+
+            return getText(result);
+        }
     }
 
 }
